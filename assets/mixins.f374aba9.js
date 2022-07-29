6e2c4c18170d21d92694c499b8a6cd891ab71e1a
PT-Mohamed-Hussein <101721780+PT-Mohamed-Hussein@users.noreply.github.com>
2022-07-29 13:40:17 +0200

    adding dist

---
+++ assets/mixins.f374aba9.js
@@ -0,0 +1,1 @@
+const s={data(){return{passwordtype:"password",passwordconfirmationtype:"password"}},methods:{changePassType(){this.passwordtype=="password"?this.passwordtype="text":this.passwordtype="password"},changePassConfirmationType(){this.passwordconfirmationtype=="password"?this.passwordconfirmationtype="text":this.passwordconfirmationtype="password"}}};export{s as m};
